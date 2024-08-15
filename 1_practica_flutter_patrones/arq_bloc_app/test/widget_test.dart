d4ad5e6c7bcaba1528c62f390caca4b47c5ba3f2
yony941 <yonny_46@hotmail.com>
2024-08-15 17:42:45 -0500

    ajuste test

--- 1_practica_flutter_patrones/arq_bloc_app/test/widget_test.dart
+++ 1_practica_flutter_patrones/arq_bloc_app/test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:arq_bloc_app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:arq_bloc_app/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
